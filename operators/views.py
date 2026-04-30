317842769768efb8a1cd27d8bd46dd23dd5bcdf2
sirnapster88 <sir.napster@yandex.ru>
2026-04-30 11:51:04 +0300

    Import Django settings in operators views

--- operators/views.py
+++ operators/views.py
@@ -5,6 +5,7 @@ import traceback
 from pathlib import Path
 from secrets import compare_digest
 
+from django.conf import settings
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
